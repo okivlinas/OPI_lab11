fe6b655e10777506b32f88d6df47f6fb2fc4831a
vanyaKov <85314590+vanyaKov@users.noreply.github.com>
2022-12-16 15:08:54 +0300

    Add files via upload

---
+++ dop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+string  weather;
+int chooseWeather()
+{
+    cin>> weather;
+    
+    if (weather == "True" || weather == "true")// условие погоды
+        cout << "go for a walk";
+    
+    else
+    {
+        cout << "do essay" << endl;
+        cout << "eat" << endl;
+        cout << "do essay";
+    }
+
+    return 0;
+}
